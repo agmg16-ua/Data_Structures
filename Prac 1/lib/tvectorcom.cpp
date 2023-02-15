4f9460dc7e948dfa778fd2b311748ae550d1053b
agmg16-ua <agmg16@alu.ua.es>
2023-02-15 19:52:15 +0100

    All methods added (friend methods no)

--- Prac 1/lib/tvectorcom.cpp
+++ Prac 1/lib/tvectorcom.cpp
@@ -3,3 +3,61 @@
 using namespace std;
 
 #include "tvectorcom.h"
+
+TVectorCom::TVectorCom() {
+
+}
+
+TVectorCom::TVectorCom(int tam) {
+
+}
+
+TVectorCom::TVectorCom(const TVectorCom &vecCom) {
+
+}
+
+TVectorCom::~TVectorCom() {
+
+}
+
+TVectorCom & TVectorCom::operator=(const TVectorCom &vecCom) {
+
+}
+
+
+bool TVectorCom::operator==(const TVectorCom &vecCom) {
+
+}
+
+bool TVectorCom::operator!=(const TVectorCom &vecCom) {
+
+}
+
+TComplejo & TVectorCom::operator[](int pos) {
+
+}
+
+TComplejo TVectorCom::operator[](int pos) const {
+
+}
+
+
+int TVectorCom::Tamano() {
+
+}
+
+int TVectorCom::Ocupadas() {
+
+}
+
+bool TVectorCom::ExisteCom(const TComplejo &complejo) {
+
+}
+
+void TVectorCom::MostrarComplejos(double real) {
+
+}
+
+bool TVectorCom::Redimensionar(int tam) {
+
+}
