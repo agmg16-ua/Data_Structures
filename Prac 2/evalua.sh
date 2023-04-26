7c828222f03be100b0cf187cc18ceb34f711e92a
agmg16-ua <agmg16@alu.ua.es>
2023-04-26 11:37:01 +0200

    Añadidas pruebas unitarias

--- Prac 2/evalua.sh
+++ Prac 2/evalua.sh
@@ -3,7 +3,7 @@
 #
 
 #Si tus ficheros de prueba no comienzan por "tad", cambia "tad*.cpp" por el prefijo de tus ficheros, por ejemplo "prueba*.cpp":
-for FILE in TESTS_UNITARIOS/TABBCom-*.cpp ; do 
+for FILE in TESTS_UNITARIOS/*Prueba/tad*.cpp ; do 
 	echo "Evaluando $FILE"
 	
 	cp $FILE src/tad.cpp 
