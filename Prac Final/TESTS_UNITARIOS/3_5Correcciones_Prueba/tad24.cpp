e778790a587b4053f63350291ba300c1f87955fe
agmg16-ua <agmg16@alu.ua.es>
2023-06-05 14:32:26 +0200

    Todos los archivos recolocados

--- Prac Final/TESTS_UNITARIOS/3_5Correcciones_Prueba/tad24.cpp
+++ Prac Final/TESTS_UNITARIOS/3_5Correcciones_Prueba/tad24.cpp
@@ -15,13 +15,13 @@ main(void)
   TComplejo c4(4,4),c6(6,6),c1(1,1),c2(2,2),c3(3,3),c5(5,5),c7(7,7);
   TVectorCom l1,l2,l3;
   
-  a.Insertar(c1);
-  a.Insertar(c2);
-  a.Insertar(c3);
   a.Insertar(c4);
   a.Insertar(c5);
-  a.Insertar(c6);
   a.Insertar(c7);
+  a.Insertar(c2);
+  a.Insertar(c1);
+  a.Insertar(c3);
+  a.Insertar(c6);
 
 
   cout<<"preorden="<<a.Preorden()<<endl;
